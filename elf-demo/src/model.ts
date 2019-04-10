8758fff9f9aa253be2593777935d78c431a302a5
didilinkin <929213769@qq.com>
2019-04-10 18:12:17 +0800

    fix(elf-demo/model.ts): fix model name

--- elf-demo/src/model.ts
+++ elf-demo/src/model.ts
@@ -1,17 +1,17 @@
 const BLOCK_NAME = 'BLOCK_NAME_CAMEL_CASE' || 'elf-demo'
 
 export default {
-  namespace: BLOCK_NAME,
+  namespace: 'BLOCK_NAME_CAMEL_CASE',
 
   state: {
     routes: [
       {
-        path: `/${BLOCK_NAME}`,
+        path: `/${'BLOCK_NAME_CAMEL_CASE'}`,
         title: 'elf-demo: 首页',
         component: './pages/page-index',
       },
       {
-        path: `/${BLOCK_NAME}/content`,
+        path: `/${'BLOCK_NAME_CAMEL_CASE'}/content`,
         title: 'elf-demo: 内容页',
         component: './pages/page-content',
         // Routes: [
